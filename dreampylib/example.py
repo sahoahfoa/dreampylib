363e2cc2f5f3b56cfed741303dc71c5dccc65d48
Eli Ribble <junk@theribbles.org>
2014-05-07 13:34:02 -0600

    Tabs to spaces

--- dreampylib/example.py
+++ dreampylib/example.py
@@ -18,24 +18,24 @@ connection = dreampylib.DreampyLib(user,key)
    
 # If the connection is up, do some tests.
 if connection.IsConnected():
-	
-	# For instance, list the available commands:
-	print 'Available commands:\n ',
-	listOfCommands = connection.AvailableCommands()
-	print '\n  '.join(listOfCommands)
-	
-	print connection.dreamhost_ps.list_ps()
-	
-	# Even if defaultReturnType is 'dict', you can get the last result as a list, too.
+    
+    # For instance, list the available commands:
+    print 'Available commands:\n ',
+    listOfCommands = connection.AvailableCommands()
+    print '\n  '.join(listOfCommands)
+    
+    print connection.dreamhost_ps.list_ps()
+    
+    # Even if defaultReturnType is 'dict', you can get the last result as a list, too.
 
-	connection.dreamhost_ps.list_size_history(ps = 'ps7093')
-	print connection.ResultList()
-	
-	# Let's also print the keys so we know what we're looking at
-	print connection.ResultKeys()
-	
-	
-	#print connection.mysql.list_dbs()
+    connection.dreamhost_ps.list_size_history(ps = 'ps7093')
+    print connection.ResultList()
+    
+    # Let's also print the keys so we know what we're looking at
+    print connection.ResultKeys()
+    
+    
+    #print connection.mysql.list_dbs()
 else:
-	print "Error connecting!"
-	print connection.Status()
+    print "Error connecting!"
+    print connection.Status()
